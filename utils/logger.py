8f7c9cd175f27d36946d55dad27dd69bb42ea4c1
jeffemart <jefferson.github@gmail.com>
2024-11-21 16:12:35 -0300

    correcting routine execution

--- utils/logger.py
+++ utils/logger.py
@@ -9,7 +9,7 @@ def configure_logger():
     if not logger.hasHandlers():
         # Configura o arquivo de log
         file_handler = logging.FileHandler("app.log")
-        file_handler.setLevel(logging.ERROR)
+        file_handler.setLevel(logging.INFO)
         formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
         file_handler.setFormatter(formatter)
 
@@ -18,10 +18,10 @@ def configure_logger():
 
         # Configura o console para exibir apenas logs de erro
         console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.WARNING)
+        console_handler.setLevel(logging.INFO)
         logger.addHandler(console_handler)
 
-        # Define o nível do logger para capturar logs de DEBUG e acima
-        logger.setLevel(logging.WARNING)
+        # Define o nível do logger para capturar logs de INFO e acima
+        logger.setLevel(logging.INFO)
 
     return logger
